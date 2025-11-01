41f4c514d06aa5168b6130364e3e1432941243c2
Ruth Derksen <ruthderksen@outlook.com>
2025-10-31 21:44:44 -0400

    added functionality to IngredientOnClick function, with debug tests
    made a temporary stash & crafting area; used hardcoded CharmComponents for now

--- Assets/Scripts/CharmCreator.cs
+++ Assets/Scripts/CharmCreator.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class CharmCreator : CharmCreatorInterface
 {
@@ -14,7 +16,10 @@ public class CharmCreator : CharmCreatorInterface
 
     // Stash
     // Have a list here or something
+    public List<CharmComponent> stash = new List<CharmComponent>();
 
+    // Crafting area - can change this, wasn't sure how to implement
+    public CharmComponent[] craftingArea = new CharmComponent[2];
 
     // Look in the charmscreatorinterface class
     void CharmCreatorInterface.CraftButtonOnClick()
@@ -24,7 +29,39 @@ public class CharmCreator : CharmCreatorInterface
 
     void CharmCreatorInterface.IngredientOnClick()
     {
-        throw new System.NotImplementedException();
+        // Don't know how to access the scriptable object that got clicked
+        CharmComponent clickedObject = new CharmComponent();
+        clickedObject.componentType = CharmComponent.ComponentType.EVIL_EYE;
+        // Did this rather than check for null
+        CharmComponent emptyComponent = new CharmComponent();
+        clickedObject.componentType = CharmComponent.ComponentType.EMPTY;
+
+        if (!clickedObject.isInStash)
+        {
+            craftingArea[System.Array.LastIndexOf(craftingArea,clickedObject)]=emptyComponent;
+            Debug.Log("Removed " + clickedObject.componentType + " from crafting area");
+            stash.Add(clickedObject);
+            Debug.Log("Added " + clickedObject.componentType + " to stash");
+            clickedObject.isInStash = false;
+        }
+        else
+        {
+            // Unsure how we're implementing the crafting area so I just made an array up by the stash
+            if (craftingArea[0].componentType == CharmComponent.ComponentType.EMPTY)
+            {
+                craftingArea[0] = clickedObject;
+                Debug.Log("Added " + clickedObject.componentType + " to first crafting slot");
+            }
+            else if (craftingArea[1].componentType == CharmComponent.ComponentType.EMPTY)
+            {
+                craftingArea[1] = clickedObject;
+                Debug.Log("Added " + clickedObject.componentType + " to second crafting slot");
+            }
+            else
+            {
+                Debug.Log("an oopsy daisy happened :(");
+            }
+        }
     }
 
     void CharmCreatorInterface.OnCraftFail()
